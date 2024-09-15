8efae36f43f2ff3444357a89e584908421a0be43
TheTVS <tiagoseraphim03@gmail.com>
2024-09-15 17:14:18 -0300

    teste

--- newTemp.php
+++ newTemp.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="resource/styles/temp.css">
+    <link rel="stylesheet" type="text/css" href="resource/styles/temp.css">
     <title>Document</title>
 </head>
 <body>
@@ -11,27 +11,37 @@
     <a href="admin.php">voltar</a>
     </div>
     <div class="conteudo">
+        <table>
+            <tr>
             <form id="form"> 
                 <label for="temp_name">Nome da temporada: </label>
                 <input type="text" name="" id="temp_name">
                 <br>
+            </tr>
+            <tr>
                 <label for="temp_init">Data de início: </label>
-                <input type="date" name="" id="temp_init">
-                <br>
-                <label for="temp_end">Data de término: </label>
+                <input type="date" name="" id="temp_init"> 
+                <label for="temp_end"> Data de término: </label>
                 <input type="date" name="" id="temp_end">
                 <br>
+            </tr>
                 <label for="temp_val">Valor da inscrição (Inteira)</label>
                 <input type="number" step="0.01" name="" id="temp_val">
                 <br>
+            <tr>
                 <label for="temp_parc">Numero maximo de parcelas aceitas: </label>
                 <input type="number" name="" id="temp_parc">
                 <br>
-                <label for="temp_festas">Festas da temporada </lable><br>
+            </tr>
+            <tr>
+                <label for="temp_festas">Festas da temporada: </lable><br>
                 <textarea id="temp_festas" name="story" rows="5" cols="33" maxlength="200"></textarea>
                 <br>
+            <tr>
                 <input type="submit" class="btn">
+            </tr>
             </form>
+        </table>
     </div>
 </body>
 </html>

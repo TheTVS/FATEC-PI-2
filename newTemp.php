3f9662a19b2c344ff5f0d66ba5091fb18d5d5659
TheTVS <tiagoseraphim03@gmail.com>
2024-09-15 17:22:50 -0300

    mano

--- newTemp.php
+++ newTemp.php
@@ -13,32 +13,44 @@
     <div class="conteudo">
         <table>
             <tr>
-            <form id="form"> 
-                <label for="temp_name">Nome da temporada: </label>
-                <input type="text" name="" id="temp_name">
-                <br>
+                <th>
+                    <form id="form"> 
+                        <label for="temp_name">Nome da temporada: </label>
+                        <input type="text" name="" id="temp_name">
+                        <br>
+                </th>
+            </tr>
+            <tr>   
+                <th>
+                        <label for="temp_init">Data de início: </label>
+                        <input type="date" name="" id="temp_init"> 
+                        <label for="temp_end"> Data de término: </label>
+                        <input type="date" name="" id="temp_end">
+                        <br>
+                </th>
+            </tr>
+                <th>
+                        <label for="temp_val">Valor da inscrição (Inteira)</label>
+                        <input type="number" step="0.01" name="" id="temp_val">
+                        <br>
+                </th>
+            <tr>
+                <th>
+                        <label for="temp_parc">Numero maximo de parcelas aceitas: </label>
+                        <input type="number" name="" id="temp_parc">
+                        <br>
+                </th>
             </tr>
             <tr>
-                <label for="temp_init">Data de início: </label>
-                <input type="date" name="" id="temp_init"> 
-                <label for="temp_end"> Data de término: </label>
-                <input type="date" name="" id="temp_end">
-                <br>
-            </tr>
-                <label for="temp_val">Valor da inscrição (Inteira)</label>
-                <input type="number" step="0.01" name="" id="temp_val">
-                <br>
+                <th>
+                        <label for="temp_festas">Festas da temporada: </lable><br>
+                        <textarea id="temp_festas" name="story" rows="5" cols="33" maxlength="200"></textarea>
+                        <br>
+                </th>
             <tr>
-                <label for="temp_parc">Numero maximo de parcelas aceitas: </label>
-                <input type="number" name="" id="temp_parc">
-                <br>
-            </tr>
-            <tr>
-                <label for="temp_festas">Festas da temporada: </lable><br>
-                <textarea id="temp_festas" name="story" rows="5" cols="33" maxlength="200"></textarea>
-                <br>
-            <tr>
-                <input type="submit" class="btn">
+                <th>
+                        <input type="submit" class="btn">
+                </th>
             </tr>
             </form>
         </table>

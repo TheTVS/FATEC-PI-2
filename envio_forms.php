977cca3647efedcf79e7c16d908646f2c29d8af9
TheTVS <tiagoseraphim03@gmail.com>
2024-09-21 16:22:40 -0300

    comentado os posts

--- envio_forms.php
+++ envio_forms.php
@@ -127,4 +127,104 @@ VALUES
 ('Broncodilatador'),
 ('Insulina');
 
+    $resNom = isset($_POST['res-nom']) ? $_POST['res-nom'] : '';
+    $resSob = isset($_POST['res-sob']) ? $_POST['res-sob'] : '';
+    $inputDocumento = isset($_POST['input-documento']) ? $_POST['input-documento'] : '';
+    $resCpf = isset($_POST['res-cpf']) ? $_POST['res-cpf'] : '';
+    $resTel1 = isset($_POST['res-tel-1']) ? $_POST['res-tel-1'] : '';
+    $resTel2 = isset($_POST['res-tel-2']) ? $_POST['res-tel-2'] : '';
+    $resTel3 = isset($_POST['res-tel-3']) ? $_POST['res-tel-3'] : '';
+    $resEml1 = isset($_POST['res-eml-1']) ? $_POST['res-eml-1'] : '';
+    $resEml2 = isset($_POST['res-eml-2']) ? $_POST['res-eml-2'] : '';
+    $resRes = isset($_POST['res-res']) ? $_POST['res-res'] : '';
+    $endCep = isset($_POST['end-cep']) ? $_POST['end-cep'] : '';
+    $endRua = isset($_POST['end-rua']) ? $_POST['end-rua'] : '';
+    $endBai = isset($_POST['end-bai']) ? $_POST['end-bai'] : '';
+    $endCid = isset($_POST['end-cid']) ? $_POST['end-cid'] : '';
+    $endUf = isset($_POST['end-uf']) ? $_POST['end-uf'] : '';
+    $endNum = isset($_POST['end-num']) ? $_POST['end-num'] : '';
+    $criNom = isset($_POST['cri-nom']) ? $_POST['cri-nom'] : '';
+    $criSob = isset($_POST['cri-sob']) ? $_POST['cri-sob'] : '';
+    $criRg = isset($_POST['cri-rg']) ? $_POST['cri-rg'] : '';
+    $sexo = isset($_POST['sexo']) ? $_POST['sexo'] : '';
+    $birthday = isset($_POST['birthday']) ? $_POST['birthday'] : '';
+    $criTar = isset($_POST['cri-tar']) ? $_POST['cri-tar'] : '';
+    $valPar = isset($_POST['val-par']) ? $_POST['val-par'] : '';
+
+        // Captura os valores dos campos
+    $sangue = $_POST['sangue'] ?? '';
+    $rh = $_POST['rh'] ?? '';
+    $tem_con = $_POST['tem-con'] ?? '';
+    $con_nom = $_POST['con-nom'] ?? '';
+    $con_cnt = $_POST['con-cnt'] ?? '';
+    $tem_con_obs = $_POST['tem-con-obs'] ?? '';
+    
+    // Alergias a medicamentos
+    $ale_med_aps = isset($_POST['ale-med-aps']) ? 'Sim' : 'Não';
+    $ale_med_mel = isset($_POST['ale-med-mel']) ? 'Sim' : 'Não';
+    $ale_med_nov = isset($_POST['ale-med-nov']) ? 'Sim' : 'Não';
+    $ale_med_pla = isset($_POST['ale-med-pla']) ? 'Sim' : 'Não';
+    $ale_med_dra = isset($_POST['ale-med-dra']) ? 'Sim' : 'Não';
+    $ale_med_pov = isset($_POST['ale-med-pov']) ? 'Sim' : 'Não';
+    $ale_med_cat = isset($_POST['ale-med-cat']) ? 'Sim' : 'Não';
+    $ale_med_pen = isset($_POST['ale-med-pen']) ? 'Sim' : 'Não';
+
+    // Outras alergias
+    $tem_ale_med = $_POST['tem-ale-med'] ?? '';
+    $ale_med_obs = $_POST['ale-med-obs'] ?? '';
+
+     // Doença Crônica
+    $temDoe = isset($_POST['tem-doe']) ? $_POST['tem-doe'] : '';
+    $doencas = [];
+    
+    if (isset($_POST['doe'])) {
+        $doencas = $_POST['doe'];
+    }
+
+    // Detalhes das doenças crônicas
+    $doeConObs = isset($_POST['doe-con-obs']) ? $_POST['doe-con-obs'] : '';
+    $doeCarObs = isset($_POST['doe-car-obs']) ? $_POST['doe-car-obs'] : '';
+    $doeDesObs = isset($_POST['doe-des-obs']) ? $_POST['doe-des-obs'] : '';
+    $doeDiaObs = isset($_POST['doe-dia-obs']) ? $_POST['doe-dia-obs'] : '';
+    $doeHemObs = isset($_POST['doe-hem-obs']) ? $_POST['doe-hem-obs'] : '';
+    $doeHipObs = isset($_POST['doe-hip-obs']) ? $_POST['doe-hip-obs'] : '';
+    $doeEnxObs = isset($_POST['doe-enx-obs']) ? $_POST['doe-enx-obs'] : '';
+    $doeBroObs = isset($_POST['doe-bro-obs']) ? $_POST['doe-bro-obs'] : '';
+    $doeDisObs = isset($_POST['doe-dis-obs']) ? $_POST['doe-dis-obs'] : '';
+    $doeOutObs = isset($_POST['doe-out-obs']) ? $_POST['doe-out-obs'] : '';
+
+    // Histórico de Saúde
+    $varicela = isset($_POST['his-sau-var']) ? $_POST['his-sau-var'] : '';
+    $sonambulismo = isset($_POST['his-sau-son']) ? $_POST['his-sau-son'] : '';
+    $sonambulismoObs = isset($_POST['his-sau-son-obs']) ? $_POST['his-sau-son-obs'] : '';
+
+     // Captura os dados do formulário
+    $sintomasMedicamentos = isset($_POST['reg-sin-med-obs']) ? $_POST['reg-sin-med-obs'] : '';
+    
+    // Para checkboxes, você pode verificar se estão definidos
+    $vacinas = [];
+    $vacinas['bcg'] = isset($_POST['his-vac-bcg']) ? true : false;
+    $vacinas['hpb'] = isset($_POST['his-vac-hpb']) ? true : false;
+    $vacinas['tet'] = isset($_POST['his-vac-tet']) ? true : false;
+    $vacinas['pol'] = isset($_POST['his-vac-pol']) ? true : false;
+    $vacinas['rot'] = isset($_POST['his-vac-rot']) ? true : false;
+    $vacinas['sar'] = isset($_POST['his-vac-sar']) ? true : false;
+    $vacinas['hpa'] = isset($_POST['his-vac-hpa']) ? true : false;
+    $vacinas['var'] = isset($_POST['his-vac-var']) ? true : false;
+    $vacinas['men'] = isset($_POST['his-vac-men']) ? true : false;
+    $vacinas['pne'] = isset($_POST['his-vac-pne']) ? true : false;
+    $vacinas['inf'] = isset($_POST['his-vac-inf']) ? true : false;
+    $vacinas['feb'] = isset($_POST['his-vac-feb']) ? true : false;
+
+    // Captura se o participante faz uso de outros medicamentos
+    $outrosMedicamentos = isset($_POST['tem-his-vac-out']) ? $_POST['tem-his-vac-out'] : '';
+    $detalhesOutrosMedicamentos = isset($_POST['his-vac-out']) ? $_POST['his-vac-out'] : '';
+
+    // Captura observações
+    $temObservacao = isset($_POST['tem-his-vac-obs']) ? $_POST['tem-his-vac-obs'] : '';
+    $observacoes = isset($_POST['his-vac-obs']) ? $_POST['his-vac-obs'] : '';
+
+    // Captura a concordância com as normas
+    $concordoNormas = isset($_POST['nor']) ? true : false;
+
 -->
